df00f853322f0dfb92824952bf9689b9823d3e2b
luna <jhoerr@druid.garden>
2024-02-20 23:33:56 -0800

    Dep Updates and Changed Protocol version

--- keys/src/lib.rs
+++ keys/src/lib.rs
@@ -3,6 +3,7 @@ use bip39::Mnemonic;
 use blst::min_pk::{PublicKey, SecretKey};
 use blst::{blst_bendian_from_scalar, blst_scalar, blst_scalar_from_be_bytes, blst_sk_add_n_check};
 use dg_xch_core::blockchain::sized_bytes::{hex_to_bytes, Bytes32, SizedBytes};
+use dg_xch_core::blockchain::sized_bytes::prep_hex_str;
 use dg_xch_puzzles::p2_delegated_puzzle_or_hidden_puzzle::puzzle_hash_for_pk;
 use hkdf::Hkdf;
 use sha2::Digest;
@@ -249,7 +250,7 @@ pub fn get_address(key: &SecretKey, index: u32, prefix: &str) -> Result<String, 
 
 pub fn parse_payout_address(s: &str) -> Result<String, Error> {
     if s.starts_with("xch") || s.starts_with("txch") {
-        decode_puzzle_hash(s).map(|s| s.to_string())
+        decode_puzzle_hash(s).map(|b| prep_hex_str(&b.to_string()))
     } else if s.len() == 64 {
         match hex_to_bytes(s) {
             //Should be a pointless conversion, validates the string is hex
